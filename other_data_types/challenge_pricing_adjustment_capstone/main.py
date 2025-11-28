dd7229a832b43df7bbb75eef2bf1c666c2ecd50b
mrbordean <>
2025-11-28 06:47:42 +0000

    Challenge: Pricing Adjustment Capstone

---
+++ other_data_types/challenge_pricing_adjustment_capstone/main.py
@@ -0,0 +1,29 @@
+grocery_inventory = {
+    "Milk": ("Dairy", 3.50, 8),
+    "Eggs": ("Dairy", 5.50, 30),
+    "Bread": ("Bakery", 2.99, 15),
+    "Apples": ("Produce", 1.50, 50)
+}
+
+if grocery_inventory["Eggs"][1] > 5:
+    grocery_inventory["Eggs"] = ("Dairy", 4.50, 30)
+    print("Eggs are too expensive, reducing the price by $1.")
+else:
+    print("The price of Eggs is reasonable.")
+
+grocery_inventory.update({"Tomatoes": ("Produce", 1.20, 30)})
+print("Inventory after adding Tomatoes:", grocery_inventory)
+
+if grocery_inventory["Milk"][2] < 10:
+    grocery_inventory["Milk"] = ("Dairy", 3.50, 28)
+    print("Milk needs to be restocked. Increasing stock by 20 units.")
+else:
+    print("Milk has sufficient stock.")
+
+if grocery_inventory["Apples"][1] > 2:
+    grocery_inventory.pop("Apples")
+    print("Apples removed from inventory due to high price.")
+
+print("Updated inventory:", grocery_inventory)
+    
+    
